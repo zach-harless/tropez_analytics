4eb62d80893232dd97e12cc3e82dcfa74a3c6e09
Zach Harless <zachattackking@gmail.com>
2023-07-08 17:18:07 -0700

    adding sql for glenn

---
+++ sql/search_query_follower_distribution.sql
@@ -0,0 +1,11 @@
+/*
+Local artist Lost in Tropez and Ope have scripts that operate their marketing efforts on twitter. A byproduct
+of this is a database where they store results. 
+
+This script identifies their followers and the search query that led to the user in hopes to increase followers.
+
+*/
+
+select 
+	account 
+from user_followers uf;
